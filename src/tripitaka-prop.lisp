a638cc78c119cf5f01a49db02e63a5b72e48c64f
myaosato <tetu60u@yahoo.co.jp>
2017-10-02 11:33:05 +0900

    add caching mechanism

--- src/tripitaka-prop.lisp
+++ src/tripitaka-prop.lisp
@@ -1,5 +1,7 @@
 (in-package :tripitaka)
 
+(defvar *file-cache* (make-hash-table :test 'equal))
+
 (defun name->file (name)
   (merge-pathnames *data-dir* (concatenate 'string name ".rosa")))
 
@@ -8,7 +10,10 @@
     (rosa:peruse-as-plist in #'string-upcase)))
 
 (defun name->data (name)
-  (file->dat (name->file name)))
+  (let ((date (gethash name)))
+    (if data
+        data
+        (file->dat (name->file name)))))
     
 (defun data->file (data path)
   (with-open-file (out path :direction :output :if-exists :supersede)
@@ -18,7 +23,7 @@
   (with-open-file (out (name->file name) :direction :output :if-exists :supersede)
     (princ (rosa:indite data) out)))
 
-(defun get-prop (data prop &optional (type 'string))
+(defun get-prop-from-data (data prop &optional (type 'string))
   (cond ((eql type 'list) (coerce (getf data prop) 'list))
         ((eql type 'vector) (getf data prop))
         (t (concatenate 'string (getf data prop)))))
@@ -26,11 +31,11 @@
 (defun get-prop-as-list (data prop)
   (get-prop data prop 'list))
 
-(defun get-prop-by-name (name prop &optional (type 'string))
+(defun get-prop (name prop &optional (type 'string))
   (get-prop (name->data name) type))
 
-(defun get-prop-by-name-as-list (name prop &optional (type 'string))
-  (get-prop-by-name name prop 'list))
+(defun get-prop-as-list (name prop &optional (type 'string))
+  (get-prop name prop 'list))
 
 (defun set-prop (data prop value)
   (cond ((listp value) (setf (getf data prop) (coerce value 'vector)))
