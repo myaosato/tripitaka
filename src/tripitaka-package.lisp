0bb7f01f18f8809be1d77fa8d444405efdce97b7
myaosato <tetu60u@yahoo.co.jp>
2017-10-09 21:10:44 +0900

    remove export symbols temporarily

--- src/tripitaka-package.lisp
+++ src/tripitaka-package.lisp
@@ -1,19 +1,11 @@
 (in-package :cl-user)
+
 (defpackage :tripitaka
   (:use :common-lisp)
-  (:export :ready
-           :read-rc
-           :set-project
-           :make-project
-           :make-dat
-           :make-html
-           :update-page
-           :make-new-diary
-           :update-diary
-           :update-diary-with-feed
-           :update-default-feed
-           :update-atom))
+  (:export ))
+
 (in-package :tripitaka)
+
 (defvar *charset-utf8*
   #+(or sbcl ccl cmu allegro ecl lispworks) :utf-8
   #+clisp charset:utf-8)
